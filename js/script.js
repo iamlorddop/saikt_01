a4c6abb4ed6bc3ec1f57ac2506f9107b490da66c
iamlorddop <juliahavaeva7@gmail.com>
2022-09-19 20:21:01 +0300

    delete php mailer and fix adaptive

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 /* -------------------- slider for blocks -------------------- */
-let slider = document.querySelector('.blocks__items'),
-  sliderList = slider.querySelector('.blocks__slider'),
-  sliderTrack = slider.querySelector('.blocks__track'),
-  slides = slider.querySelectorAll('.block__item'),
+let slider = document.querySelector(".blocks__items"),
+  sliderList = slider.querySelector(".blocks__slider"),
+  sliderTrack = slider.querySelector(".blocks__track"),
+  slides = slider.querySelectorAll(".block__item"),
   slideWidth = slides[0].offsetWidth,
   slideIndex = 0,
   posInit = 0,
@@ -22,23 +22,23 @@ let slider = document.querySelector('.blocks__items'),
   trfRegExp = /([-0-9.]+(?=px))/,
   swipeStartTime,
   swipeEndTime,
-  getEvent = function() {
-    return (event.type.search('touch') !== -1) ? event.touches[0] : event;
+  getEvent = function () {
+    return event.type.search("touch") !== -1 ? event.touches[0] : event;
   },
-  slide = function() {
+  slide = function () {
     if (transition) {
-      sliderTrack.style.transition = 'transform .5s';
+      sliderTrack.style.transition = "transform .5s";
     }
-    sliderTrack.style.transform = `translate3d(-${slideIndex * slideWidth}px, 0px, 0px)`;
-
+    sliderTrack.style.transform = `translate3d(-${
+      slideIndex * slideWidth
+    }px, 0px, 0px)`;
   },
-  swipeStart = function() {
+  swipeStart = function () {
     let evt = getEvent();
 
     if (allowSwipe) {
-
       swipeStartTime = Date.now();
-      
+
       transition = true;
 
       nextTrf = (slideIndex + 1) * -slideWidth;
@@ -47,19 +47,18 @@ let slider = document.querySelector('.blocks__items'),
       posInit = posX1 = evt.clientX;
       posY1 = evt.clientY;
 
-      sliderTrack.style.transition = '';
+      sliderTrack.style.transition = "";
 
-      document.addEventListener('touchmove', swipeAction);
-      document.addEventListener('mousemove', swipeAction);
-      document.addEventListener('touchend', swipeEnd);
-      document.addEventListener('mouseup', swipeEnd);
+      document.addEventListener("touchmove", swipeAction);
+      document.addEventListener("mousemove", swipeAction);
+      document.addEventListener("touchend", swipeEnd);
+      document.addEventListener("mouseup", swipeEnd);
 
-      sliderList.classList.remove('grab');
-      sliderList.classList.add('grabbing');
+      sliderList.classList.remove("grab");
+      sliderList.classList.add("grabbing");
     }
   },
-  swipeAction = function() {
-
+  swipeAction = function () {
     let evt = getEvent(),
       style = sliderTrack.style.transform,
       transform = +style.match(trfRegExp)[0];
@@ -100,32 +99,39 @@ let slider = document.querySelector('.blocks__items'),
         }
       }
 
-      if (posInit > posX1 && transform < nextTrf || posInit < posX1 && transform > prevTrf) {
+      if (
+        (posInit > posX1 && transform < nextTrf) ||
+        (posInit < posX1 && transform > prevTrf)
+      ) {
         reachEdge();
         return;
       }
 
-      sliderTrack.style.transform = `translate3d(${transform - posX2}px, 0px, 0px)`;
+      sliderTrack.style.transform = `translate3d(${
+        transform - posX2
+      }px, 0px, 0px)`;
     }
-
   },
-  swipeEnd = function() {
+  swipeEnd = function () {
     posFinal = posInit - posX1;
 
     isScroll = false;
     isSwipe = false;
 
-    document.removeEventListener('touchmove', swipeAction);
-    document.removeEventListener('mousemove', swipeAction);
-    document.removeEventListener('touchend', swipeEnd);
-    document.removeEventListener('mouseup', swipeEnd);
+    document.removeEventListener("touchmove", swipeAction);
+    document.removeEventListener("mousemove", swipeAction);
+    document.removeEventListener("touchend", swipeEnd);
+    document.removeEventListener("mouseup", swipeEnd);
 
-    sliderList.classList.add('grab');
-    sliderList.classList.remove('grabbing');
+    sliderList.classList.add("grab");
+    sliderList.classList.remove("grabbing");
 
     if (allowSwipe) {
       swipeEndTime = Date.now();
-      if (Math.abs(posFinal) > posThreshold || swipeEndTime - swipeStartTime < 300) {
+      if (
+        Math.abs(posFinal) > posThreshold ||
+        swipeEndTime - swipeStartTime < 300
+      ) {
         if (posInit < posX1) {
           slideIndex--;
         } else if (posInit > posX1) {
@@ -139,13 +145,11 @@ let slider = document.querySelector('.blocks__items'),
       } else {
         allowSwipe = true;
       }
-
     } else {
       allowSwipe = true;
     }
-
   },
-  setTransform = function(transform, comapreTransform) {
+  setTransform = function (transform, comapreTransform) {
     if (transform >= comapreTransform) {
       if (transform > comapreTransform) {
         sliderTrack.style.transform = `translate3d(${comapreTransform}px, 0px, 0px)`;
@@ -153,118 +157,119 @@ let slider = document.querySelector('.blocks__items'),
     }
     allowSwipe = false;
   },
-  reachEdge = function() {
+  reachEdge = function () {
     transition = false;
     swipeEnd();
     allowSwipe = true;
   };
 
-  sliderTrack.style.transform = 'translate3d(0px, 0px, 0px)';
-  sliderList.classList.add('grab');
+sliderTrack.style.transform = "translate3d(0px, 0px, 0px)";
+sliderList.classList.add("grab");
 
-  sliderTrack.addEventListener('transitionend', () => allowSwipe = true);
-  slider.addEventListener('touchstart', swipeStart);
-  slider.addEventListener('mousedown', swipeStart);
+sliderTrack.addEventListener("transitionend", () => (allowSwipe = true));
+slider.addEventListener("touchstart", swipeStart);
+slider.addEventListener("mousedown", swipeStart);
 
 /* -------------------- slider for review -------------------- */
 let position = 0;
 const slidesToShow = 3;
 const slidesToScroll = 1;
-const container = document.querySelector('.review__row');
-const track = document.querySelector('.review__track');
-const items = document.querySelectorAll('.review__item');
-const btnPrev = document.querySelector('.prev');
-const btnNext = document.querySelector('.next');
+const container = document.querySelector(".review__row");
+const track = document.querySelector(".review__track");
+const items = document.querySelectorAll(".review__item");
+const btnPrev = document.querySelector(".prev");
+const btnNext = document.querySelector(".next");
 const itemsCount = items.length;
 const itemWidth = container.clientWidth / slidesToShow;
 const movePosition = slidesToScroll * itemWidth;
-items.forEach((item) =>{
+items.forEach((item) => {
   item.style.width = `${itemWidth}px`;
 });
-btnPrev.addEventListener('click', () => {
+btnPrev.addEventListener("click", () => {
   position += movePosition;
   setPosition();
 });
-btnNext.addEventListener('click', () => {
+btnNext.addEventListener("click", () => {
   position -= movePosition;
   setPosition();
 });
 
 const setPosition = () => {
-  track.style.transform = `translateX(${position}px)`; 
+  track.style.transform = `translateX(${position}px)`;
 };
 
 /* -------------------- form -------------------- */
-let form = document.querySelector('.js-form'),
-formInputs = document.querySelectorAll('.js-input'),
-inputEmail = document.querySelector('.js-input-email'),
-inputPhone = document.querySelector('.js-input-phone'),
-inputCheckbox = document.querySelector('.js-input-checkbox');
+let form = document.querySelector(".js-form"),
+  formInputs = document.querySelectorAll(".js-input"),
+  inputEmail = document.querySelector(".js-input-email"),
+  inputPhone = document.querySelector(".js-input-phone"),
+  inputCheckbox = document.querySelector(".js-input-checkbox");
 
 let formData = new FormData(form);
 
 function validateEmail(email) {
-	let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-   return re.test(String(email).toLowerCase());
+  let re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
 function validateCountry(country) {
-	let re = new RegExp('.co$');
-   return re.test(String(country).toLowerCase());
+  let re = new RegExp(".co$");
+  return re.test(String(country).toLowerCase());
 }
 
 function validatePhone(phone) {
-	let re = /^[0-9\s]*$/;
-	return re.test(String(phone));
+  let re = /^[0-9\s]*$/;
+  return re.test(String(phone));
 }
 
 form.onsubmit = function () {
-	let emailVal = inputEmail.value,
-	phoneVal = inputPhone.value;
-	emptyInputs = Array.from(formInputs).filter(input => input.value === '');
+  let emailVal = inputEmail.value,
+    phoneVal = inputPhone.value;
+  emptyInputs = Array.from(formInputs).filter((input) => input.value === "");
 
-	formInputs.forEach(function(input){
-		if(input.value === '') {
-			input.classList.add('error');
-		} else {
-			input.classList.remove('error');
-		}
-	});
+  formInputs.forEach(function (input) {
+    if (input.value === "") {
+      input.classList.add("error");
+    } else {
+      input.classList.remove("error");
+    }
+  });
 
-	if(emptyInputs.length !== 0) {
-		console.log("inputs not fielled");
-		return false;
-	}
+  if (emptyInputs.length !== 0) {
+    console.log("inputs not fielled");
+    return false;
+  }
 
-	if(!validateEmail(emailVal)) {
-		console.log('email not valid');
-		inputEmail.classList.add('error');
-		return false;
-	} else {
-		inputEmail.classList.remove('error');	
-	}
+  if (!validateEmail(emailVal)) {
+    console.log("email not valid");
+    inputEmail.classList.add("error");
+    return false;
+  } else {
+    inputEmail.classList.remove("error");
+  }
 
-	if(validateCountry(emailVal)) {
-		console.log('email not valid');
-		inputEmail.classList.add('error');
-		return false;
-	} else {
-		inputEmail.classList.remove('error');
-	}
+  if (validateCountry(emailVal)) {
+    console.log("email not valid");
+    inputEmail.classList.add("error");
+    return false;
+  } else {
+    inputEmail.classList.remove("error");
+  }
 
-	if (!validatePhone(phoneVal)) {
-		console.log('phone not valid');
-		inputPhone.classList.add('error');
-		return false;
-	} else {
-		inputPhone.classList.remove('error');
-	}
+  if (!validatePhone(phoneVal)) {
+    console.log("phone not valid");
+    inputPhone.classList.add("error");
+    return false;
+  } else {
+    inputPhone.classList.remove("error");
+  }
 
-	if(!inputCheckbox.checked){
-		console.log('checkbox not valid');
-		inputCheckbox.classList.add('error');
-		return false;
-	} else {
-		inputEmail.classList.remove('error');	
-	}
-}
+  if (!inputCheckbox.checked) {
+    console.log("checkbox not valid");
+    inputCheckbox.classList.add("error");
+    return false;
+  } else {
+    inputEmail.classList.remove("error");
+  }
+};
--- sendemail.php
+++ sendemail.php
@@ -1,23 +1,23 @@
 <?php
-	$to = 'juliahavaeva7@gmail.com';
-	$from = trim($_POST['email']);
+//	$to = 'mail@mail.com';
+//	$from = trim($_POST['email']);
 
-	$body = '<h1>Заявка</h1>' . "\r\n";
+//	$body = '<h1>Заявка</h1>' . "\r\n";
 
-	if(trim(!empty($_POST['name']))){
-		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>'."\r\n";
-	}
-	if(trim(!empty($_POST['tel']))){
-		$body.='<p><strong>Телефон:</strong> '.$_POST['tel'].'</p>'."\r\n";
-	}
-	if(trim(!empty($_POST['email']))){
-		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>'."\r\n";
-	}
-	$body .= "X-Mailer: PHP/" . phpversion();
+//	if(trim(!empty($_POST['name']))){
+//		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>'."\r\n";
+//	}
+//	if(trim(!empty($_POST['tel']))){
+//		$body.='<p><strong>Телефон:</strong> '.$_POST['tel'].'</p>'."\r\n";
+//	}
+//	if(trim(!empty($_POST['email']))){
+//		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>'."\r\n";
+//	}
+//	$body .= "X-Mailer: PHP/" . phpversion();
 
-	if(mail($to, $from, $body)){
-		echo 'Заявка отправлена';	
-	} else {
-		echo 'Заявка не отправлена';	
-	}
+//	if(mail($to, $from, $body)){
+//		echo 'Заявка отправлена';	
+//	} else {
+//		echo 'Заявка не отправлена';	
+//	}
 ?>
